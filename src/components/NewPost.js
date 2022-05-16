a960c8ed41a75c1de53cf3c3d2743a4baf122430
Josue Garcia <josue.garcia809@outlook.com>
2022-05-16 16:40:51 -0500

    Added the newform Functionality

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,9 +1,33 @@
 import React from "react";
 
-export const NewPost = () => {
+export const NewPost = ({
+  handleSubmit,
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+}) => {
   return (
-    <main>
-      <h1>NewPost</h1>
+    <main className="NewPost">
+      <h2>NewPost</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          type="text"
+          required
+          value={postTitle}
+          onChange={e => setPostTitle(e.target.value)}
+          id="postTitle"
+        />
+        <label htmlFor="postBody">Post:</label>
+        <textarea
+          id="postBody"
+          required
+          value={postBody}
+          onChange={e => setPostBody(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 };
